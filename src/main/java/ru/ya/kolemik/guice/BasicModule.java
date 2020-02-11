eff198d6359e0049e19d941448542acf1860523e
Kolesnikov Mikhail <mixa_@mail.ru>
2020-02-11 15:24:45 +0300

    Revert "test some intercepter"

--- src/main/java/ru/ya/kolemik/guice/BasicModule.java
+++ src/main/java/ru/ya/kolemik/guice/BasicModule.java
@@ -1,15 +1,8 @@
 package ru.ya.kolemik.guice;
 
-import java.util.Arrays;
-
-import org.aopalliance.intercept.MethodInterceptor;
-import org.aopalliance.intercept.MethodInvocation;
-
 import com.google.inject.AbstractModule;
-import com.google.inject.matcher.Matchers;
 import com.google.inject.name.Names;
 
-import ru.ya.kolemik.guice.api.BP;
 import ru.ya.kolemik.guice.api.Config;
 import ru.ya.kolemik.guice.api.Counter;
 import ru.ya.kolemik.guice.api.Default;
@@ -32,28 +25,5 @@ public class BasicModule extends AbstractModule {
         bind(Counter.class).toProvider(CounterProvider.class);
         bind(Counter.class).annotatedWith(Default.class).to(CounterAtomic.class);
         bind(Hello.class).to(HelloSimple.class);
-        MethodInterceptor interceptor = new MethodInterceptor() {
-            @Override
-            public Object invoke(MethodInvocation invocation) throws Throwable {
-                System.err.println(invocation.getMethod().getAnnotation(BP.class).value());
-                System.err.println(invocation.getMethod().getName() + Arrays.toString(invocation.getArguments()));
-                Throwable t = null;
-                try {
-                    return invocation.proceed();
-                } catch (Throwable e) {
-                    t = e;
-                    throw t;
-                } finally {
-                    if (t != null) {
-                        System.err.println("ERROR: " + t);
-                        t.printStackTrace();
-                    } else {
-                        System.err.println("SUCCESS");
-                    }
-                }
-            }
-        };
-        bindInterceptor(Matchers.subclassesOf(Hello.class), Matchers.annotatedWith(BP.class), interceptor);
-        bindInterceptor(Matchers.subclassesOf(Counter.class), Matchers.annotatedWith(BP.class), interceptor);
     }
 }
--- src/main/java/ru/ya/kolemik/guice/impl/IntAppender.java
+++
@@ -1,9 +0,0 @@
-package ru.ya.kolemik.guice.impl;
-
-public class IntAppender extends AbstractAppender<Integer> {
-
-    @Override
-    protected Integer accumulator(Integer a, Integer b) {
-        return a!=null?a + b:b;
-    }
-}
